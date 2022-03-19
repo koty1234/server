fe356a6135a2b80ffeef9ba5c6407b289fd0dbeb
koty1234 <info@onlinesolutionsforyou.ca>
2022-03-19 10:39:09 -0400

    add port env for express.listen

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ app.use(cors({
 );
 app.use(cookieParser());
 
-app.listen(5000, () => console.log("Server started on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server started on port" +PORT));
 
 //set up routers
 
