0e325980a0dae7606f68370dce75c882e6a7c77d
koty1234 <info@onlinesolutionsforyou.ca>
2022-03-24 08:22:48 -0400

    changed cors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:3000","https://snippetmanagerak.netlify.app"] ,
+    origin: ["http://localhost:3000","https://credcheck-server.herokuapp.com"] ,
     credentials: true,
 })
 );
